ab7aadf3d95d00cc4085efe137fc119091c16375
Nicolai <nime1312@gmail.com>
2017-09-20 15:14:45 +0200

    filters.c/h fixes

--- Code/filters.h
+++ Code/filters.h
@@ -2,11 +2,11 @@
 #define FILTERS_H
 
 // Feel free to change return statement and arguments
-void lowPassFilter(int* input[], int* output[], int n);
-void highPassFilter(int* input[], int* output[], int n);
-void derivativeFilter(int* input[], int* output[], int n);
-void squareFilter(int* output[], int n);
-void movingWindowIntegration(int* input[], int* output[], int n);
+void lowPassFilter(int input[], int output[], int n);
+void highPassFilter(int input[], int output[], int n);
+void derivativeFilter(int input[], int output[], int n);
+void squareFilter(int output[], int n);
+void movingWindowIntegration(int input[], int output[], int n);
 
 // Implement additional filters
  
