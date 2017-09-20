b49f8cf0fd34f2b282f6baabda02ea753203a097
Christian <christian@E550.eduroam.wireless.dtu.dk>
2017-09-20 15:02:44 +0200

    Filters 2. edition

--- Code/filters.c
+++ Code/filters.c
@@ -3,8 +3,7 @@
 
 int offset = 10;
 
-void lowPassFilter(int* input[], int* output[], int n)
-{
+void lowPassFilter(int* input[], int* output[], int n){
     /*
     y_n = 2*y_(n-1) - y_(n-2) + (1/32)*(x_n - 2*x_(n-6) + x_(n-12))
     */
@@ -12,7 +11,7 @@ void lowPassFilter(int* input[], int* output[], int n)
     output[n] = 2*output[n-1] - output[n-2] + (1/32)*(input[n+offset] - 2*input[n+offset-6] + input[n+offset-12]);
 }
 
-void highPassFilter(int* input[], int* output[], int n);{
+void highPassFilter(int* input[], int* output[], int n){
     /*
     y_n = y_(n-1) - x_n/32 + x_(n-16) - x_(n-17) + x_(n-32)/32
     */
@@ -36,7 +35,7 @@ void squareFilter(int* output[], int n){
     output[n] = sqrt(output[n]^2);
 }
 
-void movingWindowIntegration(int* output[], int* input[], int n){
+void movingWindowIntegration(int* input[], int* output[], int n){
     /*
     y_n = (1/N)*(x_(n-(N-1)) + x_(n-(N-2)) + ... + x_n)
     */
